17b5e8cfef7e0e0ad7154d726221f3b029ea8f07
ScriptCamilo <script.camilo@gmail.com>
2022-09-06 21:44:31 -0300

    Update github pages deploy

---
+++ precache-manifest.2031f299ed472ba1a7a8fbb0d5ed00bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0797e492ebf52e0c46577421ff6bd86",
+    "url": "/trybe-online-store/index.html"
+  },
+  {
+    "revision": "79ce54a0af1249bf7e35",
+    "url": "/trybe-online-store/static/css/main.80975c90.chunk.css"
+  },
+  {
+    "revision": "a9d74fd3913ddf7edad7",
+    "url": "/trybe-online-store/static/js/2.54f165de.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trybe-online-store/static/js/2.54f165de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ce54a0af1249bf7e35",
+    "url": "/trybe-online-store/static/js/main.d1232672.chunk.js"
+  },
+  {
+    "revision": "dd5955c9277cf9a9bcb9",
+    "url": "/trybe-online-store/static/js/runtime-main.f0f9f147.js"
+  },
+  {
+    "revision": "a7da500d691f8ee84dd7df3e304bfe9a",
+    "url": "/trybe-online-store/static/media/Boleto.a7da500d.jpg"
+  },
+  {
+    "revision": "130f3bdad533fd7724f011797ae3878a",
+    "url": "/trybe-online-store/static/media/Cartao.130f3bda.jpg"
+  },
+  {
+    "revision": "f59560a64d2baf5784fd659071e44bc3",
+    "url": "/trybe-online-store/static/media/EstrelaDourada.f59560a6.png"
+  }
+]);
